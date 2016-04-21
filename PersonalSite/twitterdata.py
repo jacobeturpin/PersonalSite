e0d5b9078934d9c563003459fb31f9a51fe9fa76
jacobeturpin <jacobeturpin@gmail.com>
2016-04-20 20:22:14 -0400

    Added initial python-twitter code
    Implemented very basic code to testing the basic usage of the
    python-twitter package. The package appears to have issues with
    importing related to python 3 compatibility. It'll be necessary to
    either upgrade to the next version's RC or look for another package.

--- PersonalSite/twitterdata.py
+++ PersonalSite/twitterdata.py
@@ -1,6 +1,13 @@
 import twitter
+from config import *
 
-api = twitter.Api(consumer_key='consumer_key',
-                  consumer_secret='consumer_secret',
-                  access_token_key='access_token',
-                  access_token_secret='access_token_secret')
+api = twitter.Api(consumer_key=TWITTER_CONSUMER_KEY,
+                  consumer_secret=TWITTER_CONSUMER_SECRET,
+                  access_token_key=TWITTER_ACCESS_TOKEN,
+                  access_token_secret=TWITTER_ACCESS_TOKEN_SECRET)
+
+if __name__ == '__main__':
+    print(api)
+
+    test = api.GetUserTimeline()
+    print(test)
