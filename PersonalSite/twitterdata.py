630f30c35525f964468951dda15df09ca5ec0ad9
jacobeturpin <jacobeturpin@gmail.com>
2016-04-24 18:23:34 -0400

    Implemented basic test of twitter package
    Able to successfully test the 'twitter' package by returning
    timeline/feed data. Because of the compatibility issues that exists with
    the 'python-twitter' package, it's likely this one was will supplement
    it (despite less ease-of-use) for the time being.

--- PersonalSite/twitterdata.py
+++ PersonalSite/twitterdata.py
@@ -1,6 +1,11 @@
 import twitter
 from config import *
 
+
+''' ---------------------- '''
+''' PYTHON-TWITTER PACKAGE '''
+''' ---------------------- '''
+'''
 api = twitter.Api(consumer_key=TWITTER_CONSUMER_KEY,
                   consumer_secret=TWITTER_CONSUMER_SECRET,
                   access_token_key=TWITTER_ACCESS_TOKEN,
@@ -11,3 +16,19 @@ if __name__ == '__main__':
 
     test = api.GetUserTimeline()
     print(test)
+'''
+
+
+''' --------------- '''
+''' TWITTER PACKAGE '''
+''' --------------- '''
+
+if __name__ == '__main__':
+    
+    t = twitter.Twitter(auth=twitter.OAuth(TWITTER_ACCESS_TOKEN, 
+                                           TWITTER_ACCESS_TOKEN_SECRET,
+                                           TWITTER_CONSUMER_KEY, 
+                                           TWITTER_CONSUMER_SECRET))
+    test = t.statuses.home_timeline()
+    print(len(test))
+    print(test[0]['user'])
