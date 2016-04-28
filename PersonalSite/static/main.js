1892fca7792da08d3da4ec124272805ee0a0e7b7
jacobeturpin <jacobeturpin@gmail.com>
2016-04-28 18:00:50 -0400

    Beginnings of adding AJAX capabilities to load profile data

--- PersonalSite/static/main.js
+++ PersonalSite/static/main.js
@@ -1,1 +1,11 @@
 ﻿// Script to go here
+
+$(function () {
+    $('a#twitter').bind('click', function () {
+        $.getJSON('/GetTwitterData', {},
+            function (data) {
+            $("#result").text(data.result);
+        });
+        return false;
+    });
+});
